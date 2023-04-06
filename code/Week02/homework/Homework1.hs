be35e68087f5bc2e3d6b0f57571ffba72932c099
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-04-06 07:47:49 +0300

    finished homework1

--- code/Week02/homework/Homework1.hs
+++ code/Week02/homework/Homework1.hs
@@ -7,19 +7,20 @@ module Homework1 where
 
 import qualified Plutus.V2.Ledger.Api as PlutusV2
 import           PlutusTx             (compile)
-import           PlutusTx.Prelude     (Bool (..), BuiltinData)
+import           PlutusTx.Prelude     (Bool (..), BuiltinData,traceIfFalse,($),Eq ((==)),(&&))
 import           Utilities            (wrapValidator)
-
+import           Prelude              (IO)
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
 
 {-# INLINABLE mkValidator #-}
 -- This should validate if and only if the two Booleans in the redeemer are True!
 mkValidator :: () -> (Bool, Bool) -> PlutusV2.ScriptContext -> Bool
-mkValidator _ _ _ = False
+mkValidator _ (a,b) _ = (a && b) ==True
 
 wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 wrappedVal = wrapValidator mkValidator
+
 
 validator :: PlutusV2.Validator
 validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| wrappedVal ||])
