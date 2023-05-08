637a49ad036894ff34356f6a523c3bd853c2a02b
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-05-08 11:22:36 +0300

    fixed Homework2

--- code/Week03/homework/Homework2.hs
+++ code/Week03/homework/Homework2.hs
@@ -7,11 +7,14 @@
 
 module Homework2 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash,
+import           Plutus.V1.Ledger.Interval (contains)
+import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash,TxInfo (txInfoValidRange),scriptContextTxInfo,
                                        ScriptContext, Validator,
-                                       mkValidatorScript)
+                                       mkValidatorScript,from)
 import           PlutusTx             (applyCode, compile, liftCode)
-import           PlutusTx.Prelude     (Bool (False), (.))
+
+import           Plutus.V2.Ledger.Contexts (txSignedBy,txInfoSignatories)   
+import           PlutusTx.Prelude     (Bool (False), (.),($),(&&))
 import           Utilities            (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
@@ -20,7 +23,19 @@ import           Utilities            (wrapValidator)
 {-# INLINABLE mkParameterizedVestingValidator #-}
 -- This should validate if the transaction has a signature from the parameterized beneficiary and the deadline has passed.
 mkParameterizedVestingValidator :: PubKeyHash -> POSIXTime -> () -> ScriptContext -> Bool
-mkParameterizedVestingValidator _beneficiary _deadline () _ctx = False -- FIX ME!
+mkParameterizedVestingValidator beneficiary deadline () ctx = signedBybeneficiary && deadlineReached
+      where
+    info :: TxInfo
+    info = scriptContextTxInfo ctx
+
+    signedBybeneficiary :: Bool
+    signedBybeneficiary = txSignedBy info beneficiary
+
+    deadlineReached :: Bool
+    deadlineReached = (contains (from ( deadline ) ) $ txInfoValidRange info)
+
+
+    
 
 {-# INLINABLE  mkWrappedParameterizedVestingValidator #-}
 mkWrappedParameterizedVestingValidator :: PubKeyHash -> BuiltinData -> BuiltinData -> BuiltinData -> ()
@@ -28,3 +43,7 @@ mkWrappedParameterizedVestingValidator = wrapValidator . mkParameterizedVestingV
 
 validator :: PubKeyHash -> Validator
 validator beneficiary = mkValidatorScript ($$(compile [|| mkWrappedParameterizedVestingValidator ||]) `applyCode` liftCode beneficiary)
+
+
+
+     
